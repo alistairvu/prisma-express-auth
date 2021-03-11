ced9893e8eb6874b811a67a9dad585e82183c85d
Alistair Vu <alistairvu@gmail.com>
2021-03-11 12:44:43 +0700

    Prepare for deploy

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import userRouter from "./routes/userRoutes"
+import path from "path"
 
 dotenv.config()
 
@@ -9,6 +10,20 @@ const port = process.env.PORT || 6960
 
 app.use(express.json())
 app.use("/api/users", userRouter)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(path.resolve(), "frontend", "build", "index.html")
+    )
+  )
+} else {
+  app.get("/api", (req, res) => {
+    res.send("API is running...")
+  })
+}
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)
